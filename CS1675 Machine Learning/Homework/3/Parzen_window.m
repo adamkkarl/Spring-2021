c96dedd1ad2c96d500bda1ecfc76148b7785f20a
adamkkarl <adamkkarl@gmail.com>
2021-02-18 10:13:02 -0500

    p3 almost done

--- CS1675 Machine Learning/Homework/3/Parzen_window.m
+++ CS1675 Machine Learning/Homework/3/Parzen_window.m
@@ -1,15 +1,17 @@
 function [densityx] = Parzen_window(x,h,D)
 %calculate a probability est given x using a Parzen window
-    for i=1:N
-        temp = D(i,:);
-        temp = abs(temp - x);
-        
-%         fprintf('Debug:: temp=');
-%         disp(temp);
-        if temp <= v
-            disp('temp < v');
-            K = K+1;
-        end
-    end
-end
+    N=length(D);
+    V=h;         %since centered on x
 
+    [densityx] = []
+    for i=1:length(x)
+        K=0;
+        for j=1:length(D)
+            if (x(i)-(h/2)) < D(j)
+                if D(j) < (x(i)+(h/2))
+                    K=K+1;   %increment number in region
+                end
+            end
+        end
+        densityx = [densityx K/(N*V)]
+    end
