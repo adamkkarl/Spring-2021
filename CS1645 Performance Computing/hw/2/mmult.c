5cf53f42821b4dab015407dd7cc240e82f427798
Adam Karl <adamkkarl@gmail.com>
2021-02-25 01:42:17 -0500

    mmult parallelized

--- CS1645 Performance Computing/hw/2/mmult.c
+++ CS1645 Performance Computing/hw/2/mmult.c
@@ -6,7 +6,7 @@
 
 #define		NROW	1024
 #define		NCOL	NROW
-#define		NUM_THREADS	1
+#define		NUM_THREADS	64
 
 
 #define TEST_RESULTS
@@ -24,6 +24,29 @@ int outputArrayC [NROW][NCOL];
 struct timeval startTime;
 struct timeval finishTime;
 double timeIntervalLength;
+
+void *parallelCalc(void *threadid) {
+	long tid;
+	tid = (long)threadid;
+
+	int chunk_rows = NROW/NUM_THREADS;
+
+	int start_row = chunk_rows*tid;
+	int end_row = start_row+chunk_rows;
+
+	for(int row=start_row; row<end_row; row++)
+	{
+		for(int col=0; col<NCOL; col++)
+		{
+			int mySum = 0;
+			for(int i=0; i<NROW; i++) {
+				mySum += inputArrayA[row][i]*Weight[i][col];
+			}
+			mySum += inputArrayB[row][col]; //only add once
+			outputArrayC[row][col] += mySum;
+		}
+	}
+}
 
 
 int main(int argc, char* argv[])
@@ -56,50 +79,45 @@ int main(int argc, char* argv[])
 	long t;
 	void *status;
 
-	// CREATE MULTIPLE THREADS
-	for(t=0; t<NUM_THREADS; t++){
-		//printf("In main: creating thread %ld\n", t);
 
-		rc = pthread_create(&threads[t], NULL, NULL, (void *)t);
+	//W*A + B
+//	for(i=0;i<NROW;i++)
+//	{
+//		for(j=0; j<NCOL; j++)
+//		{
+//			for(k=0;k<NROW;k++)
+//			{
+//				outputArrayC[i][j]+=inputArrayA[i][k]*Weight[k][j];
+//			}
 
 
 
-		//W*A + B
-		for(i=0;i<NROW;i++)
-		{
-			for(j=0; j<NCOL; j++)
-			{
-				for(k=0;k<NROW;k++)
-				{
-					outputArrayC[i][j]+=inputArrayA[i][k]*Weight[k][j];
+			// CREATE MULTIPLE THREADS
+			for(t=0; t<NUM_THREADS; t++){
+				//printf("In main: creating thread %ld\n", t);
+
+				rc = pthread_create(&threads[t], NULL, parallelCalc, (void *)t);
+
+				if (rc){
+					printf("ERROR; return code from pthread_create() is %d\n", rc);
+					exit(-1);
 				}
-				outputArrayC[i][j]+=inputArrayB[i][j];
 			}
-		}
 
-		if (rc){
-			printf("ERROR; return code from pthread_create() is %d\n", rc);
-			exit(-1);
-		}
-	}
+
+
+
+//			outputArrayC[i][j]+=inputArrayB[i][j];
+//		}
+//	}
+
+
 
 
 	// JOIN THREADS
 	for(t=0; t<NUM_THREADS; t++) {
 		rc = pthread_join(threads[t], &status);
 	}
-
-
-
-
-
-
-
-
-
-
-
-
 
 	//=========================================================
 	//Get the end time
