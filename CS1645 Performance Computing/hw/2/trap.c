98bd27adc6835a6947a746c35c6bda3e0e12d444
Adam Karl <adamkkarl@gmail.com>
2021-02-25 12:44:10 -0500

    finished parallelizing trap.c

--- CS1645 Performance Computing/hw/2/trap.c
+++ CS1645 Performance Computing/hw/2/trap.c
@@ -1,4 +1,6 @@
+#include <pthread.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include <time.h>
 #include <sys/time.h>
@@ -12,43 +14,91 @@
 struct timeval startTime;
 struct timeval finishTime;
 double timeIntervalLength;
+double finalArea;
 
 int NUM_THREADS = 8;
 
+void *parallelCalc(void *threadid) {
+	long tid;
+	tid = (long)threadid;
+
+	int chunk_size = NSTEPS/NUM_THREADS;
+	double h = (double)(P_END-P_START)/NSTEPS;
+
+	double start = (double) (P_END-P_START)*tid/NUM_THREADS;
+	double end = (double) (P_END-P_START)*(tid+1)/NUM_THREADS;
+
+	double area = 0.0;
+	double f_result;
+
+	double p_current;
+
+	//very first height only added once
+	if(tid==0) {
+		f_result = cos(start);
+		area -= f_result*h/2;
+	}
+
+	//add each height twice
+	for(int i = 0; i < chunk_size; i++)
+	{
+		p_current = start + i*h;
+		f_result = cos(p_current);
+		area += f_result*h; //2*f_result*h/2 == f_result*h
+		p_current += h;
+	}
+
+	//very last height only added once
+	if(tid==NUM_THREADS-1) {
+		f_result = cos(end);
+		area -= f_result*h/2;
+	}
+
+	finalArea += area;
+}
+
+
 int main(int argc, char* argv[])
 {
-
-	int i;
-	double h;
-	double area;
-	double p_current = P_START;
-	double f_result_low, f_result_high;
-
 	//
 	//I N I T I A L I Z A T I O N S
 	//
-	h = (double)(P_END-P_START)/NSTEPS;
-	p_current = P_START;
-	area=0.0;
+	finalArea = 0.0;
 
 
 	//Get the start time
 	gettimeofday(&startTime, NULL);
+	//=================================================================
 
-	for(i = 0; i < NITER; i++)
-	{
-		p_current = i*h;
-		f_result_low = cos(p_current);
-		f_result_high = cos(p_current+h);
-		area += (f_result_low + f_result_high)*h/2;
-		p_current += h;
+	// DECLARE THREADS
+	pthread_t threads[NUM_THREADS];
+	int rc;
+	long t;
+	void *status;
+
+	// CREATE MULTIPLE THREADS
+	for(t=0; t<NUM_THREADS; t++){
+		//printf("In main: creating thread %ld\n", t);
+
+		rc = pthread_create(&threads[t], NULL, parallelCalc, (void *)t);
+
+		if (rc){
+			printf("ERROR; return code from pthread_create() is %d\n", rc);
+			exit(-1);
+		}
 	}
 
+	// JOIN THREADS
+	for(t=0; t<NUM_THREADS; t++) {
+		rc = pthread_join(threads[t], &status);
+	}
+
+	//=================================================================
 	//Get the end time
 	gettimeofday(&finishTime, NULL);  /* after time */
 
 
-	printf("Result : %.2lf \n",area);
+	printf("Result : %.2lf \n",finalArea);
 
 	//Calculate the interval length
 	timeIntervalLength = (double)(finishTime.tv_sec-startTime.tv_sec) * 1000000
