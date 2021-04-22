6b52dfe961354a2daf2c5c9cebeaf2c6c1587fd3
adamkkarl <adamkkarl@gmail.com>
2021-04-22 14:43:57 -0400

    cleaned up code

--- CS1675 Machine Learning/Homework/11/main_1c.m
+++ CS1675 Machine Learning/Homework/11/main_1c.m
@@ -3,7 +3,7 @@ function [V_pi_hist] = main_1c
 RL_init_model();
 state = 0;
 
-policy = [1; 1; 3; 2];
+policy = [1, 1, 3, 2];
 
 hist = [];
 action = 0;
@@ -19,17 +19,18 @@ for i=1:15000
         V_pi_hist = [V_pi_hist; step, V_pi];
     end
     if mod(step, 200) == 0
+        %reset model
         state = RL_reset_environment(model);
     end
 
-    action = policy(state, 1);
+    action = policy(state);
     n_s(state) = n_s(state) + 1;
     
     [new_state, reward] = RL_simulate_one_step(state, action, model);
     
-    alpha = 1./((1+n_s).^0.6);
+    alpha = 1/((1+n_s(state))^0.6);
     
-    V_pi(state) = (1-alpha(state))*V_pi(state) + alpha(state)*(reward+gamma*V_pi(new_state));
+    V_pi(state) = (1-alpha)*V_pi(state) + alpha*(reward+gamma*V_pi(new_state));
     
     disp(V_pi);
     state = new_state;
