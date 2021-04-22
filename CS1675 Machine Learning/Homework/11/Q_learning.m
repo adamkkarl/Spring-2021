000c65cd5f46529986cf035a632731f6767ceeaa
adamkkarl <adamkkarl@gmail.com>
2021-04-21 23:18:08 -0400

    1e unfinished

---
+++ CS1675 Machine Learning/Homework/11/Q_learning.m
@@ -0,0 +1,51 @@
+function [Q] = Q_learning
+
+RL_init_model();
+state = 0;
+
+action = 0;
+step = 0;
+gamma = 0.95;
+V_pi = [0,0,0,0];
+V_pi_hist = [];
+
+n_sa =[0,0,0; 0,0,0; 0,0,0; 0,0,0];
+Q    =[0,0,0; 0,0,0; 0,0,0; 0,0,0];
+
+for i=1:150000
+    if mod(step, 100) == 0
+        %record V_pi
+        V_pi_hist = [V_pi_hist; step, V_pi];
+    end
+    if mod(step, 200) == 0
+        state = RL_reset_environment(model);
+    end
+    
+    %choose action
+    p = [0.2, 0.2, 0.2];
+    max_action = find(Q(state,:) == Q(state,:), 1);
+    p(max_action) = 0.6;
+    disp("===");
+    disp(p);
+    X = rand * sum(p); %account for ties
+    if X < p(1)
+        action = 1;
+    elseif X < p(1) + p(2)
+        action = 2;
+    else
+        action = 3;
+    end
+
+    [new_state, reward] = RL_simulate_one_step(state, action, model);
+    
+    n_sa(state, action) = n_sa(state, action) + 1;
+    alpha = 1/((1+n_sa(state, action))^0.6);
+    Q(state, action) = (1-alpha)*Q(state, action) + alpha*(reward+gamma*max(Q(new_state,:)));
+    
+    state = new_state;
+    step = step+1;
+end
+    %V_pi_hist = [V_pi_hist; step, V_pi];
+    disp(Q);
+    disp(n_sa);
+end
